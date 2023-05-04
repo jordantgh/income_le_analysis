d95c356e666ce075e056c25a23718cf5d5016fad
Jordan Taylor <jtaylor81284@hotmail.com>
2023-05-04 12:41:54 +0100

    Populate main with script box modules

--- code/main.r
+++ code/main.r
@@ -1,3 +1,11 @@
 # this script will orchestrate all the other scripts
 # and set up the environment
 
+project_root <- here::here()
+options(box.path = project_root)
+
+box::use(code/county/industries/convert_naics_codes_txt_to_csv)
+box::use(code/county/industries/map_industries_to_counties)
+box::use(code/county/industries/map_dominant_industries_to_covariates)
+box::use(code/db/init_db)
+box::use(code/plots/gini_vs_le_gap)
