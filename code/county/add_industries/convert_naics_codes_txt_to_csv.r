df59e4fe505535cea21bbb390692ae30ccc336c9
Jordan Taylor <jtaylor81284@hotmail.com>
2023-05-04 13:05:14 +0100

    Rename for clarity

---
+++ code/county/add_industries/convert_naics_codes_txt_to_csv.r
@@ -0,0 +1,16 @@
+box::use(utils[read.delim, write.csv], glue[g = glue])
+
+
+dir <- g("{globalenv()$project_root}/data/external_data/industry")
+fname <- "naics_codes_1998_to_2002"
+
+system(g("wget https://www2.census.gov/programs-surveys/susb/technical-documentation/{fname}.txt -O {dir}/{fname}.txt")) #nolint
+
+system(g("awk 'NR>5 {{code=$1; $1=\"\"; gsub(/^[[:space:]]+/, \"\", $0); print code \"\t\" $0}}' {dir}/naics_codes_1998_to_2002.txt > {dir}/{fname}.tsv")) #nolint
+
+data <- read.delim(g("{dir}/{fname}.tsv"),
+  header = FALSE,
+  col.names = c("NAICS_Code", "Description")
+)
+
+write.csv(data, g("{dir}/{fname}.csv"), row.names = FALSE)
---
+++ code/county/add_industries/map_dominant_industries_to_covariates.r
@@ -0,0 +1,14 @@
+box::use(dplyr[...], readr[...], glue[g = glue])
+
+dir <- g("{globalenv()$project_root}/data")
+covariates <- read_csv(g("{dir}/chetty_online_tables/county/t12_countCovariates.csv")) #nolint
+
+industries <- read_csv(g("{dir}/derived_tables/county/county_dominant_industries_2001.csv")) #nolint
+
+# remove nonessential columns from industries
+industries <- industries %>%
+    select(-industry_naics_code)
+
+covariates <- left_join(covariates, industries, by = "cty")
+
+write_csv(covariates, g("{dir}/derived_tables/county/countyCovariates_with_industries.csv")) #nolint
