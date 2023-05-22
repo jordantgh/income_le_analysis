e32a51551e8d601be7ffe1fdf7cfd3498a918e7a
Jordan Taylor <jtaylor81284@hotmail.com>
2023-05-22 22:03:07 +0100

    Update county industry data to sqlite

--- code/county/add_industries/get_industry_data_cbp2001.r
+++ code/county/add_industries/get_industry_data_cbp2001.r
@@ -1,7 +1,26 @@
-box::use(glue[g = glue])
+box::use(
+    glue[g = glue],
+    DBI[dbConnect, dbWriteTable,dbExistsTable, dbDisconnect],
+    RSQLite[SQLite],
+    readr[read_csv]
+)
 
 dir <- g("{here::here()}/data/external_data/industry")
-fname <- "cbp01co"
+url <- g("https://www2.census.gov/programs-surveys/cbp/datasets/2001/cbp01co.zip") # nolint
 
-system(g("wget https://www2.census.gov/programs-surveys/cbp/datasets/2001/{fname}.zip -O {dir}/{fname}.zip")) # nolint
+system(g("wget {url} -O {dir}/{fname}.zip"))
 system(g("unzip {dir}/{fname}.zip -d {dir}"))
+system(g("mv {dir}/{fname}.txt {dir}/{fname}.csv"))
+
+db_file <- g("{dir}/income_le.sqlite")
+table_name <- "complete_county_industries_2001"
+
+db <- dbConnect(SQLite(), db_file)
+
+if (!dbExistsTable(db, table_name)) {
+    dbWriteTable(db, table_name, read_csv(g("{dir}/{fname}.csv")))
+    dbDisconnect(db)
+} else {
+    cat(g("Table {table_name} already exists in the database. Skipping import.\n")) # nolint
+    dbDisconnect(db)
+}
