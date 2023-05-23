120d58ecde226c4b83d3eac307c34c06f2491415
Jordan Taylor <jtaylor81284@hotmail.com>
2023-05-23 15:26:40 +0100

    Rename and switch to sqlite

--- code/county/add_industries/add_naics_data_to_db.r
+++ code/county/add_industries/add_naics_data_to_db.r
@@ -1,6 +1,7 @@
 box::use(
-  utils[read.delim, write.csv],
+  utils[read.delim],
   glue[g = glue],
+  magrittr[`%>%`],
   DBI[dbConnect, dbWriteTable, dbExistsTable, dbDisconnect],
   RSQLite[SQLite]
 )
@@ -8,9 +9,24 @@ box::use(
 dir <- g("{here::here()}/data/external_data/industry")
 fname <- "naics_codes_1998_to_2002"
 
-system(g("wget https://www2.census.gov/programs-surveys/susb/technical-documentation/{fname}.txt -O {dir}/{fname}.txt")) # nolint
+paste0(
+  "wget ",
+  "https://www2.census.gov/programs-surveys/susb/technical-documentation/",
+  "{fname}.txt ",
+  "-O {dir}/{fname}.txt"
+) %>%
+  g() %>%
+  system()
 
-system(g("awk 'NR>5 {{code=$1; $1=\"\"; gsub(/^[[:space:]]+/, \"\", $0); print code \"\t\" $0}}' {dir}/naics_codes_1998_to_2002.txt > {dir}/{fname}.tsv")) # nolint
+# we use .tsv instead of .csv because the file contains commas
+# in the description column
+paste(
+  "awk 'NR>5 {{code=$1; $1=\"\"; gsub(/^[[:space:]]+/, \"\", $0);",
+  "print code \"\t\" $0}}'",
+  "{dir}/{fname}.txt > {dir}/{fname}.tsv"
+) %>%
+  g() %>%
+  system()
 
 data <- read.delim(g("{dir}/{fname}.tsv"),
   header = FALSE,
