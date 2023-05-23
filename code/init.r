d89ab95265b5a0a5cc127f7e43add881b72381d7
Jordan Taylor <jtaylor81284@hotmail.com>
2023-05-23 15:28:31 +0100

    Add orchestrator script

---
+++ code/init.r
@@ -0,0 +1,7 @@
+box::use(
+    ./code/db/init_db[...],
+    ./code/county/add_industries/get_industry_data_cbp2001[...],
+    ./code/county/add_industries/add_naics_data_to_db[...],
+    ./code/county/add_industries/map_industries_to_counties[...],
+    ./code/county/add_industries/map_dominant_industries_to_covariates[...]
+)
