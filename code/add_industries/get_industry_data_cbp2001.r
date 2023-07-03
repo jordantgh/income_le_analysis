aa07ff62e13beaa9f50c954fa285f77710fddbce
Jordan Taylor <jtaylor81284@hotmail.com>
2023-07-03 15:10:41 +0100

    Make unzip not overwrite

--- code/add_industries/get_industry_data_cbp2001.r
+++ code/add_industries/get_industry_data_cbp2001.r
@@ -9,7 +9,7 @@ dir <- g("{here::here()}/data/external_data/industry")
 url <- g("https://www2.census.gov/programs-surveys/cbp/datasets/2001/cbp01co.zip") # nolint
 
 system(g("wget {url} -O {dir}/cbp01co.zip"))
-system(g("unzip {dir}/cbp01co.zip -d {dir}"))
+system(g("unzip -n {dir}/cbp01co.zip -d {dir}"))
 
 db_file <- g("{here::here()}/income_le.sqlite")
 table_name <- "complete_county_industries_2001"
